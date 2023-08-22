7d7d74ad8c70eaa73eb21af656d90355f0c6cf00
Htet-Wai-Yan-HWY <htetwaiyan.zyme@gmail.com>
2023-08-23 02:49:19 +0630

    day::2 almost got robot_description

--- chefbot_description/launch/description.launch.py
+++ chefbot_description/launch/description.launch.py
@@ -6,26 +6,21 @@ from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
-import xacro
 
 def generate_launch_description():
-    # gazebo_pkg = get_package_share_directory('black_donut_gazebo')
+    # gazebo_pkg = get_package_share_directory('rom2109_gazebo')
     # gz_ros_pkg = get_package_share_directory('gazebo_ros')
-    use_sim_time = LaunchConfiguration('use_sim_time')
-
-    doc = xacro.process_file('/home/zyme/ros_ws/black_donut_simulation/src/black_donut_simulation/black_donut_description/urdf/black_donut_library.urdf.xacro')
-    robot_desc = doc.toprettyxml(indent='  ')
-    params = {'robot_description': robot_desc}
-
-    # urdf_pkg = get_package_share_directory('black_donut_description')
-    # urdf_path= os.path.join(urdf_pkg, 'urdf', "black_donut_library.urdf.xacro")
-    # urdf = open(urdf_path).read()
+    #use_sim_time = LaunchConfiguration('use_sim_time')
+    
+    urdf_pkg = get_package_share_directory('chefbot_description')
+    urdf_path= os.path.join(urdf_pkg, 'urdf', "urdf.urdf")
+    urdf = open(urdf_path).read()
 
     robot_state_publisher_node = Node(
         name="robot_state_publisher",
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{"robot_description": params, 'use_sim_time': use_sim_time}],
+        parameters=[{"robot_description": urdf}],
     )
 
     # joint_state_node = Node(
