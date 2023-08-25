3c0b85dcbc80328518fe8aa9b12bf5971616a6c5
Htet-Wai-Yan-HWY <htetwaiyan.zyme@gmail.com>
2023-08-26 04:25:40 +0630

    Spawned_in_gazebo

--- black_donut_description/launch/description.launch.py
+++ black_donut_description/launch/description.launch.py
@@ -6,41 +6,45 @@ from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
+import xacro
 
 def generate_launch_description():
-    # gazebo_pkg = get_package_share_directory('rom2109_gazebo')
-    # gz_ros_pkg = get_package_share_directory('gazebo_ros')
-    #use_sim_time = LaunchConfiguration('use_sim_time')
-    
-    urdf_pkg = get_package_share_directory('black_donut_description')
-    urdf_path= os.path.join(urdf_pkg, 'urdf', "urdf.urdf")
-    urdf = open(urdf_path).read()
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    pkg_path = os.path.join(get_package_share_directory('black_donut_description'))
+    xacro_file = os.path.join(pkg_path,'urdf','robot.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+
+    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
 
     robot_state_publisher_node = Node(
         name="robot_state_publisher",
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{"robot_description": urdf}],
+        parameters=[params],
     )
 
-    # joint_state_node = Node(
-    #     name="joint_state_publisher",
-    #     package="joint_state_publisher",
-    #     executable="joint_state_publisher",
-    # )
-
-    # rviz_node = Node(
-    #     package='rviz2',
-    #     executable='rviz2',
-    #     arguments=['-d', os.path.join(urdf_pkg, 'rviz2', 'display.rviz')],
-    #     condition=IfCondition(LaunchConfiguration('open_rviz'))
-    # )
-
-    return LaunchDescription(
-        [
-            DeclareLaunchArgument('open_rviz', default_value='true', description='Open RViz.'),
-            robot_state_publisher_node,
-            #joint_state_node,
-            #rviz_node
-        ]
+    joint_state_node = Node(
+        name="joint_state_publisher",
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
     )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(pkg_path, 'rviz', 'black_donut.rviz')],
+        condition=IfCondition(LaunchConfiguration('open_rviz'))
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
+
+        robot_state_publisher_node,
+        joint_state_node,
+        rviz_node
+
+    ])
